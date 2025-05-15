4d1887be085877b52bc284637fc2a0e9c7337a85
Siddhant Nautiyal <71310931+WatashiwaSid@users.noreply.github.com>
2025-05-15 07:08:45 +0530

    Update kmeans.py

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -1,37 +1,27 @@
 '''
 Problem: Clustering
 Method: K-Means Clustering
+K: 2 (Derived with Elbow Method)
 Class: Unsuperivzed
 Author: Siddhant Nautiyal
 '''
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
-# Load data
-df = pd.read_csv("cluster.csv")
+df = pd.read_csv("iris.csv")
+df = df.drop('variety', axis=1)
 
-# Fit KMeans model
-kmeans = KMeans(n_clusters=2, random_state=0, n_init=10).fit(df)
-
-# Get cluster labels and centroids
+kmeans = KMeans(n_clusters=2).fit(df)
 labels = kmeans.labels_
 centroids = kmeans.cluster_centers_
 
-# Predict a new data point
-new_point = np.array([[175, 65]])
-predicted_cluster = kmeans.predict(new_point)
-print(f"Predicted cluster for {new_point.tolist()[0]}: {predicted_cluster[0]}")
-
-# Visualization
-plt.figure(figsize=(7, 7))
-plt.scatter(df.iloc[:, 0], df.iloc[:, 1], c=labels, cmap='viridis', alpha=0.7, edgecolors='k')
-plt.scatter(centroids[:, 0], centroids[:, 1], c='red', marker='X', s=200, label='Centroids')
-plt.scatter(new_point[:, 0], new_point[:, 1], c='blue', marker='P', s=150, label='New Point')
-plt.xlabel("Feature X")
-plt.ylabel("Feature Y")
+plt.scatter(df.iloc[:, 0], df.iloc[:, 1], c=labels)
+plt.scatter(centroids[:, 0], centroids[:, 1], c='blue', marker='X', s=200, label="Centroid")
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.title('Iris Clusters (K=2)')
 plt.legend()
-plt.title("K-Means Clustering")
 plt.show()
