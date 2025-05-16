7d0f384797bd74e71c2b1ac9870e7f1cba75ccd4
Siddhant Nautiyal <71310931+WatashiwaSid@users.noreply.github.com>
2025-05-16 21:44:30 +0530

    k nearest neighbours

--- classification/knn_iris_classfication.py
+++ classification/knn_iris_classfication.py
@@ -3,54 +3,32 @@ Algorithm: K-Nearest-Neighbours
 Category: Classification
 Data Set: Iris Data Set
 '''
-
-
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import preprocessing
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-
-
-#load dataset to pandas dataframe
-df = pd.read_csv('iris.csv')
 
 #feature selection
-data_x = df.drop('variety', axis=1) 
-data_y = df.variety.values
+df = pd.read_csv("iris.csv")
+x = df.drop('variety', axis=1)
+y = df['variety']
 
-#Split dataset
-x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.25, random_state=42)
+#train test split
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=40)
 
-#Feature Scaling 
-sc = preprocessing.StandardScaler()
-x_train_scaled = sc.fit_transform(x_train)
-x_test_scaled = sc.transform(x_test)
+#model train
+knn = KNeighborsClassifier(n_neighbors=5)
+knn = knn.fit(x_train, y_train)
 
-#Train the model to classify
-knn = KNeighborsClassifier()
-knn.fit(x_train_scaled, y_train)
+#predict
+y_pred = knn.predict(x_test)
+accuracy = (accuracy_score(y_test, y_pred)*100)
+print(f"Model Accuracy: {accuracy}")
 
-#Predict the test set results
-y_pred = knn.predict(x_test_scaled)
-accuracy = round((accuracy_score(y_test, y_pred)*100),2)
-print("Model Accuracy:", accuracy)
-
-#Test for a new dataset
-new_iris_data = np.array([[7.3, 2.5, 1.9, 0.33]])
-new_iris_scaled = sc.transform(new_iris_data)
-new_iris_prediction = knn.predict(new_iris_scaled)
-print("New Iris Prediction:", new_iris_prediction)
-
-#Visualization with Seaborn and Matplotlib
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x=x_test_scaled[:,0], y=x_test_scaled[:, 1], hue=y_test, palette={'Setosa':'blue', 'Versicolor':'green', 'Virginica': 'purple'}, alpha=0.6)
-plt.scatter(new_iris_scaled[:, 0], new_iris_scaled[:, 1], color='red', marker='*', s=200, label="New Iris")
-plt.title('Iris Variety Classification')
-plt.xlabel('Sepal Length (cm)')
-plt.ylabel('Sepal Width (cm)')
-plt.legend()
+#cm
+cm = confusion_matrix(y_test, y_pred, labels=knn.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=knn.classes_)
+disp.plot()
+plt.title("Iris Classification with KNN")
 plt.show()
