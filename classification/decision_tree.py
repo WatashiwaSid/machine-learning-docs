039b8dcd6c10e507a99f3b4bea2c5964ad80b78c
Siddhant Nautiyal <71310931+WatashiwaSid@users.noreply.github.com>
2025-05-16 21:56:03 +0530

    decision tree classfier

--- classification/decision_tree.py
+++ classification/decision_tree.py
@@ -1,36 +1,31 @@
+'''
+Algorithm: Decision Tree Classifier
+'''
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-#load dataset to pandas dataframe
-df = pd.read_csv('iris.csv')
-
 #feature selection
-data_x = df.drop('variety', axis=1) 
-data_y = df.variety.values
+df = pd.read_csv('iris.csv')
+x = df.drop('variety', axis=1) 
+y = df['variety']
 
 #Split dataset
-x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.25, random_state=42)
-
-#Feature Scaling 
-sc = preprocessing.StandardScaler()
-x_train_scaled = sc.fit_transform(x_train)
-x_test_scaled = sc.transform(x_test)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
 
 #Model training
 dt = DecisionTreeClassifier()
-dt.fit(x_train_scaled, y_train)
+dt.fit(x_train, y_train)
 
 #Predict the test set results
-y_pred = dt.predict(x_test_scaled)
-accuracy = round((accuracy_score(y_test, y_pred)*100),2)
+y_pred = dt.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)*100
 print("Model Accuracy:", accuracy)
 
 # Visualizing the Decision Tree
 plt.figure(figsize=(12, 8))
-plot_tree(dt, feature_names=data_x.columns, class_names=df['variety'].unique(), filled=True)
+plot_tree(dt, feature_names=x.columns, class_names=dt.classes_)
 plt.title("Decision Tree Visualization")
 plt.show()
