6ad1d1dfd5ae44b5718fa64b083cf32e95068abc
Siddhant Nautiyal <71310931+WatashiwaSid@users.noreply.github.com>
2025-05-17 08:59:01 +0530

    logistic regression simplified

--- regression/logistic_regression.py
+++ regression/logistic_regression.py
@@ -1,49 +1,32 @@
 '''
-Author - Siddhant Nautiyal
-Problem - Logistic Regression
-Class - Regression
-Category - Supervised Learning
+Algorithm: Logistic Regression Classifier
+Dataset: Iris
 '''
-
-import seaborn as sns
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('iris.csv')
-
-#remove Setosa variety from the dataframe for binary classification
-df = df[df['variety'] != 'Setosa']
-
-#Encode class labels to 0 and 1
-df['variety'] = df['variety'].map({'Versicolor':0, 'Virginica':1})
-
-#split predictors and outcome
-x = df.iloc[:,:-1]
-y = df.iloc[:,-1]
+#feature selection
+df = pd.read_csv("iris.csv")
+x = df.drop('variety', axis=1)
+y = df['variety']
 
 #train test split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=40)
-rg = LogisticRegression()
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3, random_state=30)
 
-#train
-rg.fit(x_train, y_train)
+#model train
+lr = LogisticRegression().fit(x_train, y_train)
 
-#prediction
-y_pred = rg.predict(x_test)
-
-#performance metrics
-acc = accuracy_score(y_pred, y_test)
-cm = confusion_matrix(y_test, y_pred)
-print(f"Accuracy: {round(acc*100,2)}")
+#predict
+y_pred = lr.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)*100
+print(f"Classification Accuracy {accuracy}")
 
 #visualize
-plt.figure(figsize=(6, 4))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Versicolor', 'Virginica'], yticklabels=['Versicolor', 'Virginica'])
-plt.xlabel('Predicted')
-plt.ylabel('Actual')
-plt.title('Confusion Matrix')
+cm = confusion_matrix(y_test, y_pred, labels=lr.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=lr.classes_)
+disp.plot()
+plt.title("Iris Classification with Logistic Regression")
 plt.show()
